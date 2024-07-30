f5e0195645beaa9e883899e59bc225d01d34355f
Shane Arceo <arceoshane23@gmail.com>
2024-07-30 17:57:58 -0400

    fixed docstring formatting

--- app/db.py
+++ app/db.py
@@ -4,14 +4,12 @@ df = pandas.read_json('./app/data/playlist[76][36][58].json')
 df['rating'] = None
 
 def get_songs(start=0, limit=None):
-    """ Return the songs based on the start and limit parameters.
-    """
+    """Return the songs based on the start and limit parameters."""
     return df[start:] if limit is None else df[start:start+limit]
 
 
 def get_song(title):
-    """ Return the song based on the title parameter.
-    """
+    """Return the song based on the title parameter."""
     return df.loc[df['title'] == title]
 
 
